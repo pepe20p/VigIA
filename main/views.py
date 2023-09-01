88a7b9b2fc15b8e4fe22f7645a4fd110abe5c4d6
joaopaulorocha052 <joaopaulorocha052@gmail.com>
2023-09-01 12:30:51 -0300

    Criação de mais views e templates

--- main/views.py
+++ main/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def contato(request):
+    return render(request, 'contato.hmtl')
+
+def faq(request):
+    return render(request, 'faq.hmtl')
