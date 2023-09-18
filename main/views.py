907333de14683594d3eccaba7fc70564df264049
joaopaulorohca052 <joaopaulo.paixaor@hotmail.com>
2023-09-17 22:51:05 -0300

    Adição do upload de vídeos

--- main/views.py
+++ main/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+import shutil 
 # Create your views here.
 def index(request):
+    if request.method == "POST" and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        upload = fs.url(filename)
+        return render(
+            request,
+            'index.html',
+            {'uploaded': upload},
+        )
+        
     return render(request, 'index.html')
 
 def contato(request):
