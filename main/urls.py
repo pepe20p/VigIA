72ca7dd3fb46c6e6109629fa8c9970f612018c8d
joaopaulorocha052 <joaopaulorocha052@gmail.com>
2023-09-18 16:22:46 -0300

    Correção dos urls

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from main.views import index, contato, ajuda, sobre
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', index, name='index'),
@@ -7,3 +9,5 @@ urlpatterns = [
     path('ajuda/', ajuda, name='ajuda'),
     path('sobre/', sobre, name='sobre'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
