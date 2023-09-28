044ee3af6bef0115f007b3e4b2defe0ffd49d9c4
Murilo Capozzi <murilocapozzi1@gmail.com>
2023-09-27 23:05:57 -0300

    JSON arrumado

--- IA/main.py
+++ IA/main.py
@@ -17,9 +17,11 @@ def writeToFile(events, filename):
 
     frames = []
 
-    if not os.path.exists(filename):
+    if os.path.exists(filename):
         with open(filename, "r") as file:
             frames = json.load(file)
+    else:
+        _ = open(filename, "x")
 
     for item in events:
         frames.append(item.__dict__)        
@@ -29,17 +31,11 @@ def writeToFile(events, filename):
 
 def identifyEvents(video):
 
-    eventos = []
-    i = 0
-
     # Listas que irão guardar as classes possíveis e outra que guardará apenas classes que desejamos trabalhar
     class_names = []
     util_class_names = []
-
-
     with open("coconames", "r") as f:
         class_names = [cname.strip() for cname in f.readlines()]
-
     with open("utilnames", "r") as f:
         util_class_names = [cname.strip() for cname in f.readlines()]
 
@@ -49,7 +45,6 @@ def identifyEvents(video):
     model = cv2.dnn.DetectionModel(net)
     model.setInputParams(size=(416, 416), scale=1/255) # da CFG
 
-
     # Captura de video da bibioteca do OpenCV
     cap = cv2.VideoCapture(video)
     fps = cap.get(cv2.CAP_PROP_FPS)
@@ -57,8 +52,9 @@ def identifyEvents(video):
 
     minuto_de_frame = int(fps*(0*60 + 2))
     segundo_de_frame = int(fps)
-
-    # Enquanto há frames do vídeo para processar
+    eventos = []
+    i = 0
+    
     has_video, frame = cap.read()
     while has_video:
 
@@ -70,26 +66,21 @@ def identifyEvents(video):
             if class_names[classid] in util_class_names and score >= 0.6:
                 
                 # Cria uma instancia na lista se nao ha este frame
-                # Percorre a lista de eventos para adicionar (caso ainda nao exista)
-                # a classe nova no frame certo
+                # Percorre a lista de eventos do frame para adicionar o novo
 
                 if not eventos or eventos[-1].frame < i and i % segundo_de_frame == 0:
                     eventos.append(Event(i))
 
-                
                 for evento in eventos:
                     
                     frame_evento = evento.frame
-                    
                     if frame_evento == i and class_names[classid] not in evento.events:
                         evento.add_event(class_names[classid])
 
 
         # Escreve o que pegou naquele minuto (frames que ocorreram nos 60 segundos)
         if(i % minuto_de_frame == 0 and i != 0):
-            
             writeToFile(eventos, "events.json")
-            
             eventos = []
 
         has_video, frame = cap.read()
